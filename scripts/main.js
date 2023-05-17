116191dbdf076a8b7b8d6a724e3c2dd445499409
Taras Melnykov <tarik762@ukr.net>
2023-05-17 13:57:43 +0300

    [ISSUE] fixed overlay apperance 3

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,16 @@
 //start-overlay hiding
-document.querySelector('#start-overlay-text h2').classList.add('hide');
-document.querySelector('#start-overlay-text h3').classList.add('hide');
-document.getElementById('body').classList.add('not-overflow');
-document.getElementById('start-overlay').classList.add('hide');
-document.getElementById('start-overlay').addEventListener('transitionend', e => {
-    document.getElementById('body').classList.remove('not-overflow');
-    document.getElementById('start-overlay').remove();
-});
+window.addEventListener('load',
+    e => {
+        document.querySelector('#start-overlay-text h2').classList.add('hide');
+        document.querySelector('#start-overlay-text h3').classList.add('hide');
+        document.getElementById('body').classList.add('not-overflow');
+        document.getElementById('start-overlay').classList.add('hide');
+        document.getElementById('start-overlay').addEventListener('transitionend', e => {
+            document.getElementById('body').classList.remove('not-overflow');
+            document.getElementById('start-overlay').remove();
+        });
+    });
+
 
 //Catch clicks in menu > li > a
 const menu_a_selectors = document.getElementById('menu').querySelectorAll('ul li a')
